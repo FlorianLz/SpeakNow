e5d9979500d4f9efef7e6120c729247570868d1a
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2019-10-16 11:21:47 +0200

    AJout test lien amitié pour afficher le mur d'un ami.

--- traitement/poster.php
+++ traitement/poster.php
@@ -1,11 +1,12 @@
 <?php
-if(isset($_POST['message']) && !empty($_POST['message'])){
+if(isset($_POST['message']) && !empty($_POST['message']) && isset($_POST['titre']) && !empty($_POST['titre'])) {
     $id=$_SESSION['id'];
-    $message = $_POST['message'];
+    $titre=htmlspecialchars($_POST['titre']);
+    $message = htmlspecialchars($_POST['message']);
     echo $message;
     $date = date("Y-m-d H:i:s");
 
-    $sql = "INSERT INTO ecrit VALUES ('','titre','$message','$date','','$id','$id')";
+    $sql = "INSERT INTO ecrit VALUES ('','$titre','$message','$date','','$id','$id')";
     $query = $pdo->prepare($sql);
     $query->execute();
 
--- vues/mur.php
+++ vues/mur.php
@@ -19,13 +19,13 @@
             <form class="formposter" action="index.php?action=poster" method="post">
                 <h3>Nouvelle publication</h3>
                 <input type="text" name="titre" placeholder="Titre...">
-                <textarea type="text" name="message" placeholder="Message..."></textarea>
+                <textarea name="message" placeholder="Message..."></textarea>
                 <input type="submit">
             </form>
         </div> 
         <?php
 
-        $sql="SELECT * FROM ecrit WHERE idAuteur=?";
+        $sql="SELECT * FROM ecrit WHERE idAuteur=? ORDER BY dateEcrit DESC";
         $query = $pdo->prepare($sql);
         $query->execute(array($_SESSION['id']));
 
@@ -43,19 +43,55 @@
         echo '</div>';
 
     } else {
-        $id = $_GET["id"];
+        $id=$_SESSION['id'];
+        $idPers = $_GET['id'];
         // Verifions si on est amis avec cette personne
         $sql = "SELECT * FROM lien WHERE etat='ami'AND ((idUtilisateur1=? AND idUtilisateur2=?) OR ((idUtilisateur1=? AND idUtilisateur2=?)))";
-
+        $query = $pdo->prepare($sql);
+        $query->execute(array($id,$idPers,$idPers,$id));
+        $line = $query->fetch();
+        if($line == true){
+            $ok=true;
+        }
         // les deux ids à tester sont : $_GET["id"] et $_SESSION["id"]
         // A completer. Il faut récupérer une ligne, si il y en a pas ca veut dire que lon est pas ami avec cette personne
     }
+
     if($ok==false) {
         echo "Vous n êtes pas encore ami, vous ne pouvez voir son mur !!";       
     } else {
     // A completer
     // Requête de sélection des éléments dun mur
-     // SELECT * FROM ecrit WHERE idAmi=? order by dateEcrit DESC
-     // le paramètre  est le $id
+    // SELECT * FROM ecrit WHERE idAmi=? order by dateEcrit DESC
+    // le paramètre  est le $id
+
+    //je récupère les infos de l'auteur
+    if(isset($_GET['id'])){
+        $sql="SELECT * FROM utilisateurs  WHERE id=?";
+        $query = $pdo->prepare($sql);
+        $query->execute(array($idPers));
+        $line = $query->fetch();
+        $nomPers=$line['nom'];
+        $prenomPers=$line['prenom'];
+        $avatarPers=$line['avatar'];
+
+        $sql="SELECT * FROM ecrit  WHERE idAuteur=? ORDER BY dateEcrit DESC";
+        $query = $pdo->prepare($sql);
+        $query->execute(array($idPers));
+        echo '<h1>Profil de '.$prenomPers.' '.$nomPers.'</h1>';
+        echo '<div class="conteneurposts">';
+        while($line = $query->fetch()){
+            echo '<div class="postmur">
+                    <div class="auteur"><img class="imgpost" src="avatars/'.$avatarPers.'"><p>'.$prenomPers.' '.$nomPers.'</p>
+                    </div>
+                    <p class="titrepost">'.$line['titre'].'</p><br>';
+            echo $line['contenu'];
+            echo '<br>';
+            echo 'Posté le '.$line['dateEcrit'];
+            echo '</div>';
+        };
+        echo '</div>';
+        }
+    
     }
 ?>  
