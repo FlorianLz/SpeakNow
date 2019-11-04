a4810e62f6d1df28a36b33fe99f98405e83424e1
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2019-11-04 19:20:22 +0100

    Suppression des likes associés au post à supprimer.

--- traitement/supprimerpost.php
+++ traitement/supprimerpost.php
@@ -8,13 +8,16 @@ if (isset($_POST['id']) && isset($_POST['titre']) && isset($_POST['message']) &&
 
     $sql = 'DELETE FROM ecrit WHERE id=? AND titre=? AND contenu=? AND dateEcrit=? AND idAuteur=?';
     $sql1 = 'DELETE FROM commentaires WHERE idPost=?';
+    $sql2 = 'DELETE FROM aime WHERE idEcrit=?';
     // Etape 1  : preparation
     try{
         $query = $pdo->prepare($sql);
         $query1 = $pdo->prepare($sql1);
+        $query2 = $pdo->prepare($sql2);
         // Etape 2 : execution : 2 paramètres dans la requêtes !!
         $query->execute(array($id,$titre,$contenu,$date,$idconnecte));
         $query1->execute(array($id));
+        $query2->execute(array($id));
 
         if(isset($_POST['image']) && !empty($_POST['image'])){
             $fichier = './imagesposts/'.$_POST['image'];
