713cd0c21d6affb71aea794ed65b7e7ab76c186f
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2019-11-17 18:52:46 +0100

    Création de fonctions permettant l'affichage des différentes parties sur le mur et le fil.

--- divers/balises.php
+++ divers/balises.php
@@ -46,6 +46,114 @@ function message($msg) {
     $_SESSION['info'] = $msg;
 }
 
+//Affichage du bouton like
+function formlike($idpost,$idredirection,$lieuredirection,$typebouton,$action){
+    echo "<form action='index.php?action=$action' method='POST'>
+            <input type='hidden' name='idPost' value='$idpost'>
+            <input type='hidden' name='$lieuredirection' value='$idredirection'>
+            <label for='like$idpost'><i class='far fa-thumbs-up $typebouton'></i></label>
+            <input id='like$idpost' type='submit' class='inputlike'>
+            </form><br><br>";
+}
+
+//Ajout commentaire
+function formajoutcommentaire($idpost,$idredirection,$lieuredirection){
+    echo '<form method="post" action="index.php?action=ajoutcommentaire">
+         <img class="imgpost" src="avatars/'.$_SESSION['avatar'].'">
+        <textarea name="comm" placeholder="Votre commentaire..."></textarea>
+        <input type="hidden" name='.$lieuredirection.' value="'.$idredirection.'">
+        <input type="hidden" name="idpost" value="'.$idpost.'">
+        <input type="submit" value="" name="submit" id="submit'.$idpost.'"><label for="submit'.$idpost.'"><i class="fas fa-paper-plane"></i></label>
+    </form>';
+}
+//Suppression commentaire
+function formsupprimercommentaire($idpost,$idcomm,$comm,$idredirection,$lieuredirection){
+    echo '<form method="post" action="index.php?action=supprimercommentaire">
+                                <input type="hidden" name="idCommentaire" value="'.$idcomm.'">
+                                <input type="hidden" name="commentaire" value="'.$comm.'">
+                                <input type="hidden" name="idpost" value="'.$idpost.'">
+                                <input type="hidden" name='.$lieuredirection.' value="'.$idredirection.'">
+                                <label for="supprimercomm'.$idcomm.'"><i class="fas fa-times"></i></label>
+                                <input type="submit" value="" id="supprimercomm'.$idcomm.'">                                
+                                </form>';
+}
+//Affichage erreurs liees aux commentaires
+function alertecomm($lineid){
+    if(isset($_SESSION['alertecomm'.$lineid])){
+        echo $_SESSION['alertecomm'.$lineid];
+        unset($_SESSION['alertecomm'.$lineid]);
+    }
+}
+//Suppression post
+function formsupprimerpost($id,$titre,$lieuredirection,$idredirection,$contenu,$image,$date){
+    echo '<form method="post" action="index.php?action=supprimerpost">
+                    <input type="hidden" name="id" value="'.$id.'">
+                    <input type="hidden" name="titre" value="'.$titre.'">
+                    <input type="hidden" name='.$lieuredirection.' value="'.$idredirection.'">
+                    <input type="hidden" name="message" value="'.$contenu.'">';
+                    if(isset($image) && !empty($image)){
+                        echo '<input type="hidden" name="image" value="'.$image.'">';
+                    }
+                echo '<input type="hidden" name="date" value="'.$date.'">
+                    <label for="supprimer'.$id.'"><i class="fas fa-times"></i></label>
+                    <input type="submit" value="" id="supprimer'.$id.'">
+                    </form>';
+}
+
+function formajoutpost($idpers,$prenompers){
+    echo "<div class='poster'><form enctype='multipart/form-data' class='formposter' action='index.php?action=poster' method='post'>";
+    if(!empty($prenompers)){
+        echo "<h3>Ecrire un message à $prenompers</h3>";
+    }else{
+        echo "<h3>Nouvelle publication</h3>";
+    }
+    echo "<input type='text' name='titre' placeholder='Titre...'>
+    <input type='hidden' name='idpers' value='$idpers'>
+    <textarea name='message' placeholder='Message...'></textarea>
+    <div class='uploadimage'>
+        <label class='uploadfile' for='image'><i class='fas fa-image'></i></label>
+        <div class='cacherbtnfile'>
+            <input type='file' name='photo' id='image' class='inputfile'>
+        </div>
+    </div>
+    <input type='submit'>
+</form></div>";
+}
+
+function demandesrecues($idpers,$avatarpers,$prenompers,$nompers){
+    echo '<div class="ami"><a href="index.php?action=mur&id='.$idpers.'"><img class="imgami" src="avatars/'.$avatarpers.'"></a><a href="index.php?action=mur&id='.$idpers.'"><p>'.$prenompers.' '.$nompers.'</p></a>';
+            echo '<form method="post" action="index.php?action=ajoutami">
+                        <input type="hidden" name="idAmi" value="'.$idpers.'">
+                        <input type="submit" value="Accepter">
+                        </form>
+                        <form method="post" action="index.php?action=refusami">
+                        <input type="hidden" name="idAmi" value="'.$idpers.'">
+                        <input type="submit" value="Refuser">
+                        </form></div>';
+}
+
+function demandeenvoyees($idpers,$avatarpers,$prenompers,$nompers){
+    echo '<div class="ami"><a href="index.php?action=mur&id='.$idpers.'"><img class="imgami" src="avatars/'.$avatarpers.'"></a><a href="index.php?action=mur&id='.$idpers.'"><p>'.$prenompers.' '.$nompers.'</p></a>';
+            echo '<form method="post" action="index.php?action=annulerajout">
+                        <input type="hidden" name="idAmi" value="'.$idpers.'">
+                        <input type="submit" value="Annuler">
+                        </form></div>';
+}
+
+function afficherpost($idpost,$idauteur,$avatarauteur,$prenomauteur,$nomauteur,$dateecrit,$idsession,$titre,$contenu,$image,$date,$idredirection){
+    echo '<div class="postmur" id="post'.$idpost.'">
+    <div class="auteur"><div><a href="index.php?action=mur&id='.$idauteur.'"><img class="imgpost" src="avatars/'.$avatarauteur.'">
+    <div><p>'.$prenomauteur.' '.$nomauteur.'</p></a><p>'.$dateecrit.'</p></div></div>
+    <div>';
+    if($idauteur == $idsession){
+        formsupprimerpost($idpost,$titre,"murredirection",$idredirection,$contenu,$image,$date);
+    }
+    echo '</div></div>
+    <p class="titrepost">'.$titre.'</p><br>
+    <p>'.$contenu.'</p>';
+}
+
+?>
 
 
 
